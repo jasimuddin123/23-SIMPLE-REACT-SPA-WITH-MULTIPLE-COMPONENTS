2aead57c87c0883794979e17211d85fbc7f31471
jasimuddin123 <jasimuddindxn@gmail.com>
2020-06-06 05:21:41 -0700

    23.5 SETTING UP LOGO POSITION, NAVIGATION, NAV MENU

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,6 +6,12 @@ const Header = () => {
     return (
         <div className="header">
             <img src={logo} alt=""/>
+
+            <nav>
+                <a href="/Shop">Shop</a>
+                <a href="/Review"> Review</a>
+                <a href="/Manage">Manage Inventory</a> 
+            </nav>
         </div>
     );
 };
